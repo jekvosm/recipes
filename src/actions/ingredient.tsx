fa609f16f0fb9dd21166d814e129714b5ff360dd
jekvosm <yn4k-jek@yandex.ru>
2026-03-07 15:35:12 +0400

    added ingredient store, ingredient type, getIngredients action, and load ingredients in app loader

--- src/actions/ingredient.tsx
+++ src/actions/ingredient.tsx
@@ -2,7 +2,7 @@
 
 import { ingredientSchema } from '@/schema/zod'
 import prisma from '@/utils/prisma'
-import { ZodError } from 'zod'
+import { success, ZodError } from 'zod'
 
 export async function createIngredient(formData: FormData) {
   try {
@@ -36,5 +36,27 @@ export async function createIngredient(formData: FormData) {
     }
     console.error('Ошибка создания ингредиента', error)
     return { error: 'Ошибка при создании ингредиента' }
+  }
+}
+
+export async function getIngredients() {
+  try {
+    const ingredients = await prisma.ingredient.findMany()
+    return { success: true, ingredients }
+  } catch (error) {
+    console.error('Ошибка получения ингредиентов', error)
+    return { error: 'Ошибка при получении ингредиентов' }
+  }
+}
+
+export async function deleteIngredient(id: string) {
+  try {
+    const ingredient = await prisma.ingredient.delete({
+      where: { id },
+    })
+    return { success: true, ingredient }
+  } catch (error) {
+    console.error('Ошибка удаления ингредиента', error)
+    return { error: 'Ошибка при удалении ингредиента' }
   }
 }
--- src/hoc/app-loader.tsx
+++ src/hoc/app-loader.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useAuthStore } from '@/store/auth.store'
+import { useIngredientStore } from '@/store/ingredient.store'
 import { useSession } from 'next-auth/react'
 import { useEffect } from 'react'
 
@@ -10,11 +11,19 @@ interface IProps {
 
 const AppLoader = ({ children }: IProps) => {
   const { data: session, status } = useSession()
-  const { setAuthState } = useAuthStore()
+  const { isAuth, setAuthState } = useAuthStore()
+  const { loadIngredients } = useIngredientStore()
 
   useEffect(() => {
     setAuthState(status, session)
   }, [status, session, setAuthState])
+
+  useEffect(() => {
+    if (isAuth) {
+      loadIngredients()
+    }
+  }, [isAuth, loadIngredients])
+
   return <>{children}</>
 }
 
